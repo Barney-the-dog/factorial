de5d273cd49fc696491a51a8ec5958cd762ccd31
Irina Ashurova <you-ve_got@mail.ru>
2023-04-06 20:35:13 -0400

    factorial

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 // Итерационный подсчет факториала
 function factIterat(num){
-
+    let result = 1
+    for (let i = 1; i <= num; i++){
+       result = result * i;
 }
+return result
+}
+console.log(factIterat(5))
 
 // Рекурсивный расчет факториала
 function factRec(num){
- 
+    if(number === 1) { 
+        return 1; 
+      }
+      return num * factRec(num-1)
 }
